9c63e7ac17310c7eb6c853185fbcfd682f006850
Andriy Slisarchuk <andriy.slisarchuk@umwerk.com>
2019-12-03 23:04:21 +0200

    added new theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,33 @@
----
-exclude_in_search: true
-layout: null
----
-(function($) {
-    'use strict';
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip();
-        $('[data-toggle="popover"]').popover();
-        $('.popover-dismiss').popover({
-            trigger: 'focus'
-        })
+$(function () {
+
+  function initSearchBox() {
+    var pages = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title'),
+      // datumTokenizer: Bloodhound.tokenizers.whitespace,
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+
+      prefetch: baseurl + '/search.json'
     });
 
-    function bottomPos(element) {
-        return element.offset().top + element.outerHeight();
-    }
-    $(function() {
-        var promo = $(".js-td-cover");
-        if (!promo.length) {
-            return
-        }
-        var promoOffset = bottomPos(promo);
-        var navbarOffset = $('.js-navbar-scroll').offset().top;
-        var threshold = Math.ceil($('.js-navbar-scroll').outerHeight());
-        if ((promoOffset - navbarOffset) < threshold) {
-            $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
-        }
-        $(window).on('scroll', function() {
-            var navtop = $('.js-navbar-scroll').offset().top - $(window).scrollTop();
-            var promoOffset = bottomPos($('.js-td-cover'));
-            var navbarOffset = $('.js-navbar-scroll').offset().top;
-            if ((promoOffset - navbarOffset) < threshold) {
-                $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
-            } else {
-                $('.js-navbar-scroll').removeClass('navbar-bg-onscroll');
-                $('.js-navbar-scroll').addClass('navbar-bg-onscroll--fade');
-            }
-        });
+    $('#search-box').typeahead({
+      minLength: 0,
+      highlight: true
+    }, {
+        name: 'pages',
+        display: 'title',
+        source: pages
+      });
+
+    $('#search-box').bind('typeahead:select', function (ev, suggestion) {
+      window.location.href = suggestion.url;
     });
-}(jQuery));
-(function($) {
-    'use strict';
-    var Search = {
-        init: function() {
-            $(document).ready(function() {
-                $(document).on('keypress', '.td-search-input', function(e) {
-                    if (e.keyCode !== 13) {
-                        return
-                    }
-                    var query = $(this).val();
-                    var searchPage = "{{ site.url }}{{ site.baseurl }}/search/?q=" + query;
-                    document.location = searchPage;
-                    return false;
-                });
-            });
-        },
-    };
-    Search.init();
-}(jQuery));
+  }
+
+  function styleContentToMD() {
+    $('#markdown-content-container table').addClass('table');
+    $('#markdown-content-container img').addClass('img-responsive');
+  }
+
+  initSearchBox();
+  styleContentToMD();
+});
